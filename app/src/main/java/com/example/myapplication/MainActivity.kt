f2ee0a20ac8a677168b54380ce2274e5af7ca9dd
DiponAAD <diponritchil101@gmail.com>
2023-03-21 19:26:46 +0600

    Initin Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityPlayBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       binding.playBtn.setOnClickListener {
+           var intent= Intent (this, PlayActivity::class.java)
+
+           startActivity(intent)
+       }
+
+
+    }
+}
